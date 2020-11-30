54dfad71ab48333a056deca963d2c670832ecf17
Moises <moises.albarran258@my.tccd.edu>
2020-11-30 02:55:00 +0000

    Class Conversion

--- main.cpp
+++ main.cpp
@@ -6,14 +6,45 @@
 #include<iomanip>
 using namespace std; 
 
-struct MenuItem
+class MenuItem
 {
-  string name;
-  double itemCost; 
-  string desc; 
-  char addLetter; 
-  char removeLetter;
-  int count; 
+  private:
+    string name;
+    double itemCost; 
+    string desc; 
+    char addLetter; 
+    char removeLetter;
+    int count; 
+    int defaultIndex;
+  public:
+    MenuItem() {}
+    MenuItem(int defInd)
+    {
+      vector<string> defaultMenuNames = {"Green Tea", "Burrito", "Item 3", "Item 4", "Item 5", "Item 6", "Item 7"}; 
+      vector<char> defaultAddLetters = {'A', 'B', 'C', 'D', 'E', 'F', 'G'}; 
+      vector<char> defaultRemoveLetters = {'a', 'b', 'c', 'd', 'e', 'f', 'g'}; 
+
+      name = defaultMenuNames[defInd];
+      addLetter = defaultAddLetters[defInd];
+      removeLetter = defaultRemoveLetters[defInd];
+
+      itemCost = (3.00 + defInd);
+      count = 0;
+      desc = "delicious";
+    }
+    void setName(string nm) {name = nm;}
+    void setItemCost(double cost) {itemCost = cost;}
+    void setDesc(string dsc) {desc = dsc;}
+    void setAddLetter(char letter) {addLetter = letter;}
+    void setRemoveLetter(char letter) {removeLetter = letter;}
+    void setCount(int c) {count = c;}
+
+    string getName() const {return name;}
+    double getItemCost() const {return itemCost;}
+    string getDesc() const {return desc;}
+    char getAddLetter() const {return addLetter;}
+    char getRemoveLetter() const {return removeLetter;}
+    int getCount() const {return count;}
 };
 
 //function definitions
@@ -21,13 +52,13 @@ void populateMenu(vector<MenuItem> &entireMenu)
 {
   //put some default values in our Menu
   const int numItems = 7; 
-  MenuItem Item1; 
-  MenuItem Item2;
-  MenuItem Item3; 
-  MenuItem Item4;
-  MenuItem Item5;
-  MenuItem Item6;
-  MenuItem Item7;    
+  MenuItem Item1(0); 
+  MenuItem Item2(1);
+  MenuItem Item3(2); 
+  MenuItem Item4(3);
+  MenuItem Item5(4);
+  MenuItem Item6(5);
+  MenuItem Item7(6);    
 
   entireMenu.push_back(Item1); //add to the end of list the Item1
   entireMenu.push_back(Item2); //add to the end of list the Item2
@@ -36,23 +67,6 @@ void populateMenu(vector<MenuItem> &entireMenu)
   entireMenu.push_back(Item5); //add to the end of list the Item5
   entireMenu.push_back(Item6); //add to the end of list the Item6
   entireMenu.push_back(Item7); //add to the end of list the Item7
-
-  vector<string> defaultMenuNames = {"Green Tea", "Burrito", "Item 3", "Item 4", "Item 5", "Item 6", "Item 7"}; 
-  vector<char> defaultAddLetters = {'A', 'B', 'C', 'D', 'E', 'F', 'G'}; 
-  vector<char> defaultRemoveLetters = {'a', 'b', 'c', 'd', 'e', 'f', 'g'}; 
-
-  for(int i = 0; i < numItems; i++)
-  {
-    //add each item to the default list efficiently 
-    entireMenu[i].name = defaultMenuNames[i]; 
-    entireMenu[i].addLetter = defaultAddLetters[i]; 
-    entireMenu[i].removeLetter = defaultRemoveLetters[i]; 
-    entireMenu[i].itemCost = (3.00 + i); //set a random starter cost for each item
-    entireMenu[i].count = 0; //initialze all counts to 0
-    entireMenu[i].desc = "delicious"; //set all default desc to "delicous"
-  }
-
-
 }
 
 void showMenu(vector<MenuItem> &m)
@@ -62,9 +76,9 @@ void showMenu(vector<MenuItem> &m)
   cout << "ADD  \tNAME \t COST \tREMOVE\tCOUNT\tDESC"<<endl; 
   for(int i = 0; i < m.size(); i++)
   {
-    cout << m[i].addLetter << ")" << setw(10) << m[i].name 
-    << setw(5) << "$" << m[i].itemCost << setw(5) << "(" << m[i].removeLetter
-    << ")" << setw(7) << m[i].count << setw(13) << m[i].desc 
+    cout << m[i].getAddLetter() << ")" << setw(10) << m[i].getName() 
+    << setw(5) << "$" << m[i].getItemCost() << setw(5) << "(" << m[i].getRemoveLetter()
+    << ")" << setw(7) << m[i].getCount() << setw(13) << m[i].getDesc() 
     <<endl; 
   }
 
@@ -74,7 +88,7 @@ void acceptOrder(vector<MenuItem> &m)
 {
   char option = '\0';// the user-selected menu item
   double subtotal = 0.0; 
-
+  int tempCount = 0;
   do
   {
     cout << "\nPlease choose an item (x to Exit): ";
@@ -82,37 +96,40 @@ void acceptOrder(vector<MenuItem> &m)
 
     for(int i=0; i < m.size(); i++)
     {
-      if(option == m[i].addLetter)
+      tempCount = m[i].getCount();
+
+      if(option == m[i].getAddLetter())
       {
-        cout << "\nMenu Item " << m[i].addLetter << " selected."; 
-        m[i].count++; //increment the count by 1
+        cout << "\nMenu Item " << m[i].getAddLetter() << " selected."; 
+
+        m[i].setCount(tempCount + 1); //increment the count by 1
         cout << "\033[2J\033[1;1H"; //clear screen 
-        cout << "\n1 UP on " << m[i].name << endl;
-        subtotal += m[i].itemCost; //increment the subtotal by the cost of the item 
+        cout << "\n1 UP on " << m[i].getName() << endl;
+        subtotal += m[i].getItemCost(); //increment the subtotal by the cost of the item 
         showMenu(m); //show the updated menu
         cout << "\nSubtotal: $" << subtotal << endl;  
       }
-      else if(option == m[i].removeLetter)
+      else if(option == m[i].getRemoveLetter())
       {
-        cout << "\nRemove Item " << m[i].removeLetter << " selected."; 
-        if(m[i].count > 0) //subtract if and only if the count is > 0
+        cout << "\nRemove Item " << m[i].getRemoveLetter() << " selected."; 
+        if(m[i].getCount() > 0) //subtract if and only if the count is > 0
         {
-          m[i].count--; //decrement the count by 1
+          m[i].setCount(tempCount - 1); //decrement the count by 1
           cout << "\033[2J\033[1;1H"; //clear screen 
-          cout << "\n1 DOWN on " << m[i].name << endl;
-          subtotal -= m[i].itemCost; //decrement the subtotal by the cost of the item
+          cout << "\n1 DOWN on " << m[i].getName() << endl;
+          subtotal -= m[i].getItemCost(); //decrement the subtotal by the cost of the item
           showMenu(m); //show the updated menu
           cout << "\nSubtotal: $" << subtotal << endl;  
         }
         else //the the user why you blocked item removal 
         {
             
-            cout << "\nItem count must be > 0 to remove: " << m[i].name << endl;
+            cout << "\nItem count must be > 0 to remove: " << m[i].getName() << endl;
         }
       }
       else if(
-                option != m[i].addLetter && 
-                option != m[i].removeLetter &&
+                option != m[i].getAddLetter() && 
+                option != m[i].getRemoveLetter() &&
                 option != 'x' &&
                 option != 'X' 
             ) //test for all of my valid inputs
